e527953acd29f34694c0dd337da032c8b40ca914
johnietre <johnietrebus@gmail.com>
2021-10-22 08:23:43 -0500

    Update proxy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod proxy;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8000";
     env_logger::init();
-    println!("yes");
+    let proxy = proxy::Proxy::new(addr)?;
+    proxy.run().await?;
+    Ok(())
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,27 +1,30 @@
 // TODO: Log socket (stream) info
-use log::{info, warn};
+#![allow(dead_code)]
+use log::{info, trace, warn};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 use std::io::{self, Read, Write};
 use std::net::{SocketAddr, ToSocketAddrs};
-use std::sync::{Arc, RwLock, atomic::{AtomicBool, Ordering}};
+use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use tokio::net::{TcpListener, TcpStream};
 
 const BUFFER_SIZE: usize = 1 << 12;
 
 // Stream read timeout in ms
-const STREAM_READ_TIMEOUT: u64 = 100;
+const STREAM_READ_TIMEOUT: u64 = 1000;
 
+const OK: &'static str = "HTTP/1.0 200 OK\r\n\r\n";
 const BAD_REQUEST: &'static str = "HTTP/1.0 400 Bad Request\r\n\r\n";
-const NOT_FOUND: &[u8] = b"HTTP/1.0 404 Not Found\r\n\r\n";
+const NOT_FOUND: &'static str = "HTTP/1.0 404 Not Found\r\n\r\n";
 const VERSION_NOT_SUPPORTED: &'static str = "HTTP/1.0 505 Version Not Supported\r\n\r\n";
 
+#[derive(Clone)]
 pub struct Proxy {
     addr: SocketAddr,
-    routes: Arc<RwLock<HashMap<String, Route>>>,
-    running: AtomicBool,
+    servers: Arc<RwLock<HashMap<String, ServerConn>>>,
+    //running: AtomicBool,
 }
 
 impl Proxy {
@@ -36,83 +39,110 @@ impl Proxy {
         let addr;
         match addr_iter.next() {
             Some(a) => addr = a,
-            _ => panic!("Error parsing addr, no address in iterator"),
+            _ => panic!("error parsing addr, no address in iterator"),
         }
         // Create the proxy
         Ok(Proxy {
             addr,
-            routes: Arc::new(RwLock::new(HashMap::new())),
-            running: AtomicBool::new(false),
+            servers: Arc::new(RwLock::new(HashMap::new())),
         })
     }
 
-    pub async fn run(&self) -> Result<(), Box<dyn Error>> {
-        // TODO: Set running to false if bind() fails
-        match self.running.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed) {
-            Ok(_) => (),
-            Err(_) => return Err(Box::new(ProxyError {
-                what: "Proxy already running".into(),
-            })),
-        }
+    pub async fn run(self) -> Result<(), Box<dyn Error + 'static>> {
         // Create the listener
-        //let listener = TcpListener::bind(self.addr).await?;
+        // If bind() fails, set the proxy state back to not running
+        trace!("attempting to listen on {}", self.addr);
         let listener = match TcpListener::bind(self.addr).await {
             Ok(l) => l,
-            Err(e) => {
-                self.running.store(true, Ordering::Relaxed);
-                return Err(Box::new(e));
-            },
+            Err(e) => return Err(Box::new(e)),
         };
-        info!(" Starting server on {}", self.addr);
+        info!("starting server on {}", self.addr);
         loop {
             let (stream, addr) = listener.accept().await?;
-            self.handle(stream, addr);
+            let proxy = self.clone();
+            tokio::spawn(async move {
+                proxy.handle(stream, addr).await;
+            });
         }
     }
 
     async fn handle(&self, stream: TcpStream, addr: SocketAddr) {
+        let addr_str = addr.to_string();
+        trace!("handling connection from {}", addr_str);
+
         let mut buffer = [0u8; BUFFER_SIZE];
+
+        match stream.readable().await {
+            Err(_) => {
+                warn!("error awaiting readable stream");
+                return;
+            },
+            _ => (),
+        }
+        match stream.try_read(&mut buffer) {
+            Err(e) => {
+                warn!("error reading from stream: {}", e);
+                return;
+            },
+            _ => (),
+        }
+        match stream.try_write(OK.as_bytes()) {
+            Err(e) => warn!("error writing to stream: {}", e),
+            _ => (),
+        }
+
+        return;
+
         // Change the stream into a standard library TcpStream
+        trace!("converting tokio::TcpStream into std::TcpStream");
         let mut std_stream;
         match stream.into_std() {
             Ok(ss) => std_stream = ss,
             Err(e) => {
-                warn!("Error converting stream: {}", e);
+                warn!("error converting stream: {}", e);
                 return;
             },
         }
         // Change it to blocking
+        trace!("changing stream to nonblocking");
         match std_stream.set_nonblocking(false) {
             Ok(_) => (),
             Err(e) => {
-                warn!("Error setting to nonblocking: {}", e);
+                warn!("error setting to nonblocking: {}", e);
                 return;
             },
         }
+
         // Set the read timeout
+        trace!("setting stream read timeout to {} ms", STREAM_READ_TIMEOUT);
         match std_stream.set_read_timeout(Some(Duration::from_millis(STREAM_READ_TIMEOUT))) {
             Ok(_) => (),
             Err(e) => {
-                warn!("Error setting read timeout: {}", e);
+                warn!("error setting read timeout: {}", e);
                 return;
             },
         }
         // Read from the stream
+        trace!("reading from the stream");
+        #[allow(unused_mut)]
         let mut bytes_read;
         match std_stream.read(&mut buffer) {
             Ok(size) => bytes_read = size,
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock || e.kind() == io::ErrorKind::TimedOut => {
-                warn!("Stream read timed out");
+                warn!("stream read timed out");
                 return;
             },
             Err(e) => {
-                warn!("Error reading from stream: {}", e);
+                warn!("error reading from stream: {}", e);
                 return;
             },
         }
         // Parse the input
+        trace!("parsing data from the stream");
+        #[allow(unused_variables)]
+        #[allow(unused_assignments)]
         let (method, uri, version);
-        match parse_request(&buffer) {
+        match parse_request(&buffer[..bytes_read]) {
             Some((m, u, v)) => {
                 method = m;
                 uri = u;
@@ -121,20 +151,21 @@ impl Proxy {
             None => {
                 match std_stream.write(BAD_REQUEST.as_bytes()) {
                     Ok(_) => (),
-                    Err(e) => warn!("Error writing response to stream: {}", e),
+                    Err(e) => warn!("error writing response to stream: {}", e),
                 }
                 return;
             }
         }
         // Check the version
         // Only handle HTTP major 1 and minor 0 and 1
+        trace!("parsing the http version");
         match parse_http_version(version) {
             Some(version) => match version {
                 (1, 0) | (1, 1) => (),
                 _ => {
                     match std_stream.write(VERSION_NOT_SUPPORTED.as_bytes()) {
                         Ok(_) => (),
-                        Err(e) => warn!("Error writing response to stream: {}", e),
+                        Err(e) => warn!("error writing response to stream: {}", e),
                     }
                     return;
                 }
@@ -142,74 +173,89 @@ impl Proxy {
             None => {
                 match std_stream.write(VERSION_NOT_SUPPORTED.as_bytes()) {
                     Ok(_) => (),
-                    Err(e) => warn!("Error writing response to stream: {}", e),
+                    Err(e) => warn!("error writing response to stream: {}", e),
                 }
                 return;
             }
         }
-        let route;
-        match self.get_handler(uri) {
-            Some(r) => route = r,
+        // Get the server conn for the given path
+        trace!("getting the server conn for the path {}", uri);
+        #[allow(unused_assignments)]
+        let server;
+        #[allow(unreachable_patterns)]
+        match self.get_path(uri.clone()) {
+            Some(s) => server = s,
+            _ => (),
             None => {
-                match std_stream.write(NOT_FOUND) {
+                match std_stream.write(NOT_FOUND.as_bytes()) {
                     Ok(_) => (),
-                    Err(e) => warn!("Error writing response to stream: {}", e),
+                    Err(e) => warn!("error writing response to stream: {}", e),
                 }
                 return;
             },
         }
         if bytes_read == BUFFER_SIZE {
-            info!("More");
+            info!("more bytes in stream");
         }
-        info!("Route: {}", route.path.clone());
+        // Write ok response
+        match std_stream.write(OK.as_bytes()) {
+            Ok(_) => (),
+            Err(e) => warn!("error writing response to stream: {}", e),
+        }
+        return;
+        unreachable!();
+        info!("route: {}", uri);
+        info!("request:\n{}", String::from_utf8_lossy(&buffer[..bytes_read]));
     }
 
-    pub fn add_handler(&self, path: String, handler: Handler) {
-        let routes = Arc::clone(&self.routes);
-        let mut routes = routes.write().unwrap();
-        routes.insert(path.clone(), Route::new(path, handler));
+    pub fn add_path(&self, server: ServerConn) -> Result<(), ProxyError> {
+        let servers = Arc::clone(&self.servers);
+        let mut servers = servers.write().unwrap();
+        if servers.values().any(|v| v.addr == server.addr) {
+            return Err(ProxyError {
+                what: "Server address already exists".into(),
+            });
+        }
+        servers.insert(server.path.clone(), server);
+        Ok(())
     }
 
-    pub fn add_route(&self, route: Route) {
-        let routes = Arc::clone(&self.routes);
-        let mut routes = routes.write().unwrap();
-        routes.insert(route.path.clone(), route);
+    pub fn get_path(&self, path: String) -> Option<ServerConn> {
+        let servers = Arc::clone(&self.servers);
+        let servers = servers.read().unwrap();
+        servers.get(&path).cloned()
     }
 
-    pub fn get_handler(&self, path: String) -> Option<Route> {
-        let routes = Arc::clone(&self.routes);
-        let routes = routes.read().unwrap();
-        routes.get(&path).cloned()
+    pub fn get_addr(&self, addr: String) -> Option<ServerConn> {
+        let servers = Arc::clone(&self.servers);
+        let servers = servers.read().unwrap();
+        servers.values().find(|v| v.addr == addr).cloned()
     }
 
     pub fn remove_path(&self, path: String) {
-        let routes = Arc::clone(&self.routes);
-        let mut routes = routes.write().unwrap();
-        routes.remove(&path);
+        let servers = Arc::clone(&self.servers);
+        let mut servers = servers.write().unwrap();
+        servers.remove(&path);
     }
 }
 
 #[derive(Clone)]
-pub struct Route {
+pub struct ServerConn {
     path: String,
-    server_addr: String,
+    // addr: SocketAddr
+    addr: String,
     scheme: String,
-    handler: Handler,
 }
 
-impl Route {
-    pub fn new(path: String, handler: Handler) -> Self {
+impl ServerConn {
+    pub fn new(path: String, addr: String, scheme: String) -> Self {
         Self {
             path,
-            handler,
-            scheme: "http".into(),
-            server_addr: String::new(),
+            addr,
+            scheme,
         }
     }
 }
-
-// TcpStream is the client stream and the slice is the request bytes
-type Handler = fn(TcpStream, &[u8]);
 
 // Returns the request's (method, URI, HTTP Version [rest of first line])
 // Returns none if the request if malformed
